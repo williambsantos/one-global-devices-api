0d202a73bd8e047e9f3906aeef1226041705ec99
William Barbosa dos Santos <williambsantos@gmail.com>
2026-01-11 18:34:06 -0300

    feat: preparing initial unit tests

---
+++ tests/one-global-devices-api-tests/Domain/Services/DevicesCrudServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace one_global_devices_api_tests.Domain.Services
+{
+    public class DevicesCrudServiceTests
+    {
+        [Fact]
+        public async Task FetchSingleDeviceAsync_Must_Call_Repository()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public async Task FetchAllDevicesAsync_Must_Call_Repository()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public async Task FetchAllByBrandAsync_Must_Call_Repository()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public async Task FetchAllByStateAsync_Must_Call_Repository()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
