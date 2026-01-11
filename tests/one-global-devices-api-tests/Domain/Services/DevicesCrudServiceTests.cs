20bd57b295edbd7df76607d6d704220e74c637f7
William Barbosa dos Santos <williambsantos@gmail.com>
2026-01-11 18:36:36 -0300

    feat: first unit test

--- tests/one-global-devices-api-tests/Domain/Services/DevicesCrudServiceTests.cs
+++ tests/one-global-devices-api-tests/Domain/Services/DevicesCrudServiceTests.cs
@@ -1,4 +1,7 @@
 ﻿using Moq;
+using OneGlobalDevicesApi.Domain.Entities;
+using OneGlobalDevicesApi.Domain.Repositories;
+using OneGlobalDevicesApi.Domain.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +16,36 @@ namespace one_global_devices_api_tests.Domain.Services
         public async Task FetchSingleDeviceAsync_Must_Call_Repository()
         {
             // Arrange
+            var deviceId = Guid.NewGuid();
+            var cancellationToken = new CancellationToken();
+
+            var deviceRepositoryMock = new Mock<IDeviceRepository>();
+            deviceRepositoryMock
+                .Setup(repo => repo.FetchByIdAsync(deviceId, cancellationToken))
+                .ReturnsAsync(new DeviceEntity { 
+                    Id = deviceId,
+                    Name = "Iphone 17 PRO MAX",
+                    Brand = "Apple"
+                });
+
+            var databaseConnectionMock = new Mock<IDatabaseConnection>();
+
+            var devicesCrudService = new DevicesCrudService(
+                deviceRepositoryMock.Object,
+                databaseConnectionMock.Object
+            );
 
             // Act
+            var result = await devicesCrudService.FetchSingleDeviceAsync(deviceId, cancellationToken);
 
             // Assert
+            deviceRepositoryMock.Verify(
+                expression: repo => repo.FetchByIdAsync(deviceId, cancellationToken),
+                times: Times.Once
+            );
+
+            Assert.NotNull(result);
+            Assert.Equal(deviceId, result.Id);
         }
 
         [Fact]
