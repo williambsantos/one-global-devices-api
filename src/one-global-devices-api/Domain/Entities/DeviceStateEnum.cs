03843cdb655d6da872a62e535d58ebe80f830425
William Barbosa dos Santos <williambsantos@gmail.com>
2026-01-18 17:58:24 -0300

    feat: map enum to string

--- src/one-global-devices-api/Domain/Entities/DeviceStateEnum.cs
+++ src/one-global-devices-api/Domain/Entities/DeviceStateEnum.cs
@@ -1,9 +1,25 @@
-﻿namespace OneGlobalDevicesApi.Domain.Entities
+﻿using System.Runtime.Serialization;
+
+namespace OneGlobalDevicesApi.Domain.Entities
 {
     public enum DeviceStateEnum
     {
+        /// <summary>
+        /// Available
+        /// </summary>
+        [EnumMember(Value = "Available")]
         Available,
+
+        /// <summary>
+        /// In Use
+        /// </summary>
+        [EnumMember(Value = "In Use")]
         InUse,
+
+        /// <summary>
+        /// Inactive
+        /// </summary>
+        [EnumMember(Value = "Inactive")]
         Inactive
     }
 }
